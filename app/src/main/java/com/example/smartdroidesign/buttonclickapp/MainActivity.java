3013bd9f14235348aaa1fea49264f88fa22a5620
Memnoc <smartdroidesign@gmail.com>
2018-07-04 15:55:47 +0100

    few corrections and updates

--- app/src/main/java/com/example/smartdroidesign/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/example/smartdroidesign/buttonclickapp/MainActivity.java
@@ -11,10 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
-    private Button button;
     private TextView textView;
-
-    private int numTimesClick = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         userInput  = findViewById(R.id.editText);
-        button =findViewById(R.id.button);
+        userInput.setText("");
+        Button button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         textView.setMovementMethod(new ScrollingMovementMethod());
 
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numTimesClick = numTimesClick + 1;
-                    String result = "The button got tapped " + numTimesClick + " time";
-                    if (numTimesClick != 1) {
-                        result += "s";
-                    }
-                    result += "\n";
-                    textView.append(result);
-                }
+                String result = userInput.getText().toString();
+                result = result + "\n";
+                textView.append(result);
+                userInput.setText("");
+            }
         };
         button.setOnClickListener(ourOnClickListener);
     }
-
 }
